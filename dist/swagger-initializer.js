c3a880ac4bd68085502d2cce8b557393e3fef24f
Sunnyrian <wo174831@qq.com>
2022-10-13 16:44:43 +0800

    chore: CI :gem:

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -4,6 +4,24 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "https://petstore.swagger.io/v2/swagger.json",
+    urls: [
+      {
+        url: "./qipumovieapi.json",
+        name: "用户小程序api"
+      },
+      {
+        url: "./qipumovieop.json",
+        name: "管理后台api"
+      },
+      {
+        url: "./qipusupplierapi.json",
+        name: "供应商小程序api"
+      },
+      {
+        url: "./xsmovieapi.json",
+        name: "???api"
+      },
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
